178925a18062a5530c1f91d44e793f75ceb62435
AbdulRehmanAtcha <abdulrehman1239091@gmail.com>
2023-12-26 22:47:27 +0500

    Connected with previous frontend

--- Express-With-React/002-Proper Folder Structure/backend/src/index.js
+++ Express-With-React/002-Proper Folder Structure/backend/src/index.js
@@ -1,11 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import { adminRoutes } from "../src/routes/admin.js";
+import userRoutes from "../src/routes/user.js";
+
 
 const app = express();
-const port = process.env.PORT || 3000;
-
 dotenv.config({ path: "./env" });
 
+const port = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "*"],
+    credentials: true,
+  })
+);
+
+app.use("/admin", adminRoutes);
+app.use("/api/v1", userRoutes);
+
+
 app.listen(port, () => {
-  console.log("Hello");
+  console.log("Hello",port);
 });
