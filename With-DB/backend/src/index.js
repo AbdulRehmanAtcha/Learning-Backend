ec8353de64a9b4fa66a99e549dc1b97e04ef8dc5
AbdulRehmanAtcha <abdulrehman1239091@gmail.com>
2024-01-02 23:56:01 +0500

    DB Connected

--- With-DB/backend/src/index.js
+++ With-DB/backend/src/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import { adminRoutes } from "../src/routes/admin.js";
 import bodyParser from "body-parser";
 import userRoutes from "../src/routes/user.js";
-
+import DBConnectionHandler from "./DB/database.js";
 
 const app = express();
 dotenv.config({ path: "./env" });
@@ -24,7 +24,7 @@ app.use(
 app.use("/admin", adminRoutes);
 app.use("/api/v1", userRoutes);
 
-
 app.listen(port, () => {
   console.log("Hello",port);
 });
+DBConnectionHandler();
