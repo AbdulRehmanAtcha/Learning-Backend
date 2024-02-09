ce8113d0298bb21ef8664068418d94ec84b40971
AbdulRehmanAtcha <abdulrehman1239091@gmail.com>
2024-02-09 12:56:49 +0500

    Changes DONE

--- Auth/backend/src/app.js
+++ Auth/backend/src/app.js
@@ -12,4 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+// Importing Routes
+import authRouter from "./routes/auth.routes.js";
+app.use("/api/v1/auth", authRouter)
+
 export { app };
