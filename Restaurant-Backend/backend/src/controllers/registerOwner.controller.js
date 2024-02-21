0564d5bf391fd101d911b5a9c755977b7543a357
AbdulRehmanAtcha <abdulrehman1239091@gmail.com>
2024-02-21 16:41:44 +0500

    Changes DONE in owner registration

--- Restaurant-Backend/backend/src/controllers/registerOwner.controller.js
+++ Restaurant-Backend/backend/src/controllers/registerOwner.controller.js
@@ -4,11 +4,6 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const RegisterOwnerHandler = asyncHandler(async (req, res) => {
-    // TODO: Get Data from frontend
-    // TODO: Check if email or restaurant name is already there or not
-    // TODO: If email or restaurant present return
-    // TODO: Check for the role is ceo or not
-    // TODO: If not then register that owner
     const { fullName, phone, email, password, restaurantName, role } = req.body
     if ([fullName, phone, email, password, restaurantName, role].some((item) => !item || item?.trim() === "")) {
         throw new ApiError(200, "All fields are required")
@@ -26,6 +21,11 @@ export const RegisterOwnerHandler = asyncHandler(async (req, res) => {
     }
     const owner = { fullName, phone, email, password, restaurantName, role: "CEO" }
     const registerOwner = await OwnerModel.create(owner)
+    const respond = await OwnerModel.findById(registerOwner._id).select("-password -refreshToken")
 
-    return res.status(200).json(new ApiResponse(200, registerOwner, "Data Received Successfully"))
+    return res.status(200).json(new ApiResponse(200, respond, "Data Received Successfully"))
+})
+
+export const LoginOwnerHandler = asyncHandler(async (req, res) => {
+    console.log(req.body)
 })
--- Restaurant-Backend/backend/src/models/owner.model.js
+++ Restaurant-Backend/backend/src/models/owner.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 
 const ownerSchema = new Schema({
@@ -34,6 +35,9 @@ const ownerSchema = new Schema({
         type: String,
         trim: true
     },
+    refreshToken: {
+        type: String,
+    },
 
 }, {
     timestamps: true
@@ -44,5 +48,35 @@ ownerSchema.pre("save", async function (next) {
     this.password = await bcrypt.hash(this.password, 10)
     next()
 })
+
+ownerSchema.methods.generateRefreshToken = function () {
+    return (
+        jwt.sign({
+
+            _id: this._id
+
+        }, process.env.REFRESH_TOKEN_SECRET, {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        })
+    )
+}
+
+ownerSchema.methods.generateAccessToken = function () {
+    return (
+        jwt.sign(
+            {
+                name: this.fullName,
+                email: this.email,
+                _id: this._id,
+                restaurantName: this.restaurantName,
+                role: this.role
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            }
+        )
+    )
+}
 
 export const OwnerModel = mongoose.model("Owner", ownerSchema)
